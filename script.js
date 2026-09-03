1778e3a9581872b7cf0a66b0c97c8fd9059728bd
report-soft <josephkato78@gmail.com>
2026-09-03 11:02:05 +0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-const toggle=document.querySelector('.menu-toggle');
-const nav=document.querySelector('.nav');
-toggle?.addEventListener('click',()=>{const open=nav.classList.toggle('open');toggle.setAttribute('aria-expanded',open)});
-document.querySelectorAll('.nav a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')));
